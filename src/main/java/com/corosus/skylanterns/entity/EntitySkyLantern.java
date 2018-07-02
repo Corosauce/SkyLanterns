e3137a9fdb625bbe705aab7c5d5cedf9157f42ed
Corosauce <cordonfreeman@gmail.com>
2018-07-01 22:43:50 -0400

    cleanup code, remove interface since it didnt help and caused ghost lighting anyways

--- src/main/java/com/corosus/skylanterns/entity/EntitySkyLantern.java
+++ src/main/java/com/corosus/skylanterns/entity/EntitySkyLantern.java
@@ -1,13 +1,10 @@
 package com.corosus.skylanterns.entity;
 
-import CoroUtil.entity.IWorldAccessHooks;
 import com.corosus.skylanterns.CommonProxy;
 import com.corosus.skylanterns.config.ConfigSkyLanterns;
-import net.minecraft.block.BlockFence;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.EntityMoveHelper;
-import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.EnumDyeColor;
 import net.minecraft.nbt.NBTTagCompound;
@@ -26,9 +23,8 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
 import java.util.Random;
-import java.util.UUID;
 
-public class EntitySkyLantern extends EntityCreature implements IWorldAccessHooks {
+public class EntitySkyLantern extends EntityCreature {
 
 	public BlockPos posLight = new BlockPos(BlockPos.ORIGIN);
 
@@ -210,69 +206,6 @@ public class EntitySkyLantern extends EntityCreature implements IWorldAccessHook
 		return 1.0F;
 	}
 
-	static class FlyingMoveHelper extends EntityMoveHelper
-	{
-		private final EntitySkyLantern parentEntity;
-		private int courseChangeCooldown;
-
-		public FlyingMoveHelper(EntitySkyLantern ghast)
-		{
-			super(ghast);
-			this.parentEntity = ghast;
-		}
-
-		public void onUpdateMoveHelper()
-		{
-			if (this.action == EntityMoveHelper.Action.MOVE_TO)
-			{
-				double d0 = this.posX - this.parentEntity.posX;
-				double d1 = this.posY - this.parentEntity.posY;
-				double d2 = this.posZ - this.parentEntity.posZ;
-				double d3 = d0 * d0 + d1 * d1 + d2 * d2;
-
-				if (this.courseChangeCooldown-- <= 0)
-				{
-					this.courseChangeCooldown += this.parentEntity.getRNG().nextInt(5) + 2;
-					d3 = (double) MathHelper.sqrt(d3);
-
-					if (this.isNotColliding(this.posX, this.posY, this.posZ, d3))
-					{
-						this.parentEntity.motionX += d0 / d3 * 0.1D;
-						this.parentEntity.motionY += d1 / d3 * 0.1D;
-						this.parentEntity.motionZ += d2 / d3 * 0.1D;
-					}
-					else
-					{
-						this.action = EntityMoveHelper.Action.WAIT;
-					}
-				}
-			}
-		}
-
-		/**
-		 * Checks if entity bounding box is not colliding with terrain
-		 */
-		private boolean isNotColliding(double x, double y, double z, double p_179926_7_)
-		{
-			double d0 = (x - this.parentEntity.posX) / p_179926_7_;
-			double d1 = (y - this.parentEntity.posY) / p_179926_7_;
-			double d2 = (z - this.parentEntity.posZ) / p_179926_7_;
-			AxisAlignedBB axisalignedbb = this.parentEntity.getEntityBoundingBox();
-
-			for (int i = 1; (double)i < p_179926_7_; ++i)
-			{
-				axisalignedbb = axisalignedbb.offset(d0, d1, d2);
-
-				if (!this.parentEntity.world.getCollisionBoxes(this.parentEntity, axisalignedbb).isEmpty())
-				{
-					return false;
-				}
-			}
-
-			return true;
-		}
-	}
-
 	@Override
 	public float getEyeHeight() {
 		return super.getEyeHeight() - 0.5F;
@@ -305,14 +238,6 @@ public class EntitySkyLantern extends EntityCreature implements IWorldAccessHook
 		super.setDead();
 		if (!world.isRemote) {
 			clearCurrentLightBlock();
-		}
-	}
-
-	@Override
-	public void onEntityRemoved() {
-		if (!world.isRemote) {
-			//doing this in this context just glitches out lighting, it doesnt actually remove the block, and then we're not tracking it
-			//clearCurrentLightBlock();
 		}
 	}
 }
